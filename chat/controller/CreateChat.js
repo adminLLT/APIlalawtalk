d0ae80c0a0aff98bdd94d6d402d426395f9b8d33
woraphat12T <talklalaw@gmail.com>
2023-08-14 21:38:11 +0700

    update

--- chat/controller/CreateChat.js
+++ chat/controller/CreateChat.js
@@ -2,10 +2,37 @@
 const express = require('express');
 const CreateChat = express.Router();
 
-const Customer = require('../../model/user')
-
+const { Customer } = require('../../model/user')
+const { ChatData,MessageData } = require('../../model/chat')
+require('../../config/connectMongo')
 CreateChat.post('/CreateChat', async (req, res) => {
-  res.json('asdsa')
+    const user = req.body.userId
+    const lawyer = req.body.lawyerId
+
+    const autonumber = await ChatData.findOne({},null, { sort: { id: -1 } });
+
+      if(autonumber === null){
+        await ChatData.create(
+                {
+                    id:1,conversation:[{user:user,lawyer:lawyer}]
+                }
+        )
+      }else{
+
+         var idauto = autonumber.id +1
+        await ChatData.create(
+            {
+                id:idauto,conversation:[{user:user,lawyer:lawyer}]
+            })
+
+        await MessageData.create(
+            {
+                conversation_id:idauto,dataConvasation:[{sender:'',message:''}]
+            })
+      }
+
+   
+  res.json(idauto)
 });
 
 module.exports = CreateChat;    
--- model/chat.js
+++ model/chat.js
@@ -2,24 +2,20 @@ const mongoose = require('mongoose');
 
  const chatSchema = mongoose.Schema(
     {
-        id: { type: String, required: true, unique: true },
-        userName:{type:String,require:[true, "plase input name" ]},
-        passWord:{type:String,require:true},
-        nameSur:{type:String,require:true},
-        nameLast:{type:String,require:true},
-        telePhone:{type:String,require:true},
-        birthDay:{type:String,require:true},
-        picture:{type:String,require:true},
-        roleCus:{type:String,require:true}, 
-        tranScript:{type:[Object],require:true},
-        yearOld:{type:String,require:true},
-        sex:{type:String,require:true},
-        province:{type:String,require:true},
-        license:{type:String,require:true},
-        status:{type:String,require:true},
-        activeReciptWork:{type:String,require:true},
-        favorateLawGroup:{type:[Object],require:true},
-        selectedrandom:{type:Number}
+        id: { type:Number, required: true, unique: true },
+        conversation:{type:[Object],require:true},
+    },
+    {
+        timestamps:true
+    }
+ ) 
+ 
+ const messageSchema = mongoose.Schema(
+    { 
+        conversation_id:{type:String,require:true},
+        dataConvasation:{type:[Object],require:true}
+        // sender: { type: String, required: true, unique: true },
+        // message: { type: String, required: true, unique: true },
     },
     {
         timestamps:true
@@ -27,4 +23,5 @@ const mongoose = require('mongoose');
  )
 
  const ChatData = mongoose.model('ChatData',chatSchema);
- module.exports = { ChatData} ;
+ const MessageData = mongoose.model('MessageData',messageSchema);
+ module.exports = { ChatData,MessageData } ;
